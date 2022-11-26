48eb9b0dc97dfb5cb1e5413fbffe33a22964d719
modehans <monicaglezsanch@gmail.com>
2022-11-26 13:00:45 +0100

    Migrate html from kickBall project and create initial ball component

---
+++ components/mdh-ball.js
@@ -0,0 +1,24 @@
+import { LitElement, html, css } from 'lit';
+
+export class MdhKickball extends LitElement {
+  static styles = [
+    css`
+      :host {
+        display: block;
+      }
+      .ball {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        cursor: pointer;
+        background-color: plum;
+      }
+    `,
+  ];
+
+  render() {
+    let ball = html`<div class="ball"><slot></slot></div>`;
+    return ball;
+  }
+}
+customElements.define('mdh-ball', MdhKickball);
--- main.js
+++ main.js
@@ -1,1 +1,41 @@
 'use strict';
+import './components/mdh-ball.js';
+const gameScreem = document.querySelector('.js_gameScreen');
+const counterHtml = document.querySelector('.js_counter');
+
+let counter = 0;
+
+const upCounter = (ev) => {
+  if (ev.target.className.includes('plusBall')) {
+    counter = counter + 3;
+  } else if (ev.target.className.includes('blackBall')) {
+    counter = counter - 2;
+  } else if (ev.target.className.includes('standarBall')) {
+    counter = counter + 1;
+  }
+  counterHtml.textContent = 'Puntos: ' + counter;
+};
+
+const randomNum = (min, max) => {
+  return ~~(Math.random() * (max + 1 - min)) + min;
+};
+
+const removeBall = (ev) => {
+  gameScreem.removeChild(ev.target);
+};
+
+const handleClickBall = (ev) => {
+  upCounter(ev);
+  removeBall(ev);
+};
+
+const createStandarBall = () => {
+  let standarBall = document.createElement('mdh-ball');
+  standarBall.addEventListener('click', handleClickBall);
+  standarBall.classList.add('standarBall');
+  standarBall.style.top = randomNum(20, 80) + '%';
+  standarBall.style.left = randomNum(20, 80) + '%';
+  gameScreem.appendChild(standarBall);
+};
+
+setInterval(createStandarBall, 1000);
